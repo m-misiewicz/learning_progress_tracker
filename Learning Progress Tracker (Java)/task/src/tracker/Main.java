15ad01d1cff8e2f52e55bd7cb44a00fcd57413c8
doktormatte <doktormatte@googlemail.com>
2024-01-11 09:11:34 +0100

    work on task 2

--- Learning Progress Tracker (Java)/task/src/tracker/Main.java
+++ Learning Progress Tracker (Java)/task/src/tracker/Main.java
@@ -6,20 +6,31 @@ import java.util.regex.*;
 public class Main {
     public static void main(String[] args) {
 
+        
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Learning Progress Tracker");
-        String line = null;
+        baseMenu(scanner);
+        scanner.close();
+    }
+
+    private static void baseMenu(Scanner scanner) {
         while (true) {
-            line = scanner.nextLine();
+//            String line;
+            String line = scanner.nextLine();
             if (line.trim().isEmpty()) {
                 System.out.println("No input.");
-            } else if (line.equals("exit")) {
+            } else if (line.equals("add students")){
+                System.out.println("Enter student credentials or 'back' to return");
+            }
+
+
+            else if (line.equals("exit")) {
                 System.out.println("Bye!");
                 break;
             } else {
                 System.out.println("Error: unknown command! ");
             }
         }
-        scanner.close();
     }
 }
